f34200bafc8cfd5c28ef8a935f25f30c5059fc05
skiming <skiming@126.com>
2017-10-27 00:03:54 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
       <div>Hello {{name}}</div>
+      <div>{{test}}</div>
       <input type="text" v-model="name">
   </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
     data() {
         return {
-            name: 'World'
+            name: 'World',
+            test: 'asd222'
         }
     },
+    methods: {
+        foo(x: string): string {
+            return x.length
+        }
+    }
     
 }
 </script>
