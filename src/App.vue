a2fd0e85ff2725c37654f1538d9e431c1286cc04
skiming <skiming@126.com>
2017-10-26 23:57:31 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+      <div>Hello {{name}}</div>
+      <input type="text" v-model="name">
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'World'
+        }
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
